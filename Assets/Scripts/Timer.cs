39edc18293109245680bc607017f36790c493b5f
Ryan McKeever <39560065+Rkfmckee@users.noreply.github.com>
2018-06-03 18:51:03 +0100

    03/06/18

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    public float time = 10.0f;
+    public Text timerUI;
+
+    private Canvas canvas;
+    private string timerText = null;
+
+    private void Start()
+    {
+        canvas = FindObjectOfType<Canvas>();
+        int totalTimers = GameObject.FindObjectsOfType<Timer>().Length;
+
+    }
+
+    private void Update()
+    {
+        /*if (Mathf.Round(time) > 0) {
+            time -= Time.deltaTime;
+            if (timerText == null){
+                timerText = "Timer";
+            }
+            timerUI.text = timerText + ": " + Mathf.Round(time);
+        } else {
+            timerUI.enabled = false;
+            Destroy(gameObject);
+        }*/
+    }
+}
